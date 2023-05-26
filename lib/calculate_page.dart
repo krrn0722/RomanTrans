68223e7ab29a9c89a000a6e04febd3a0cb901d12
krrn0722 <mur0722@outlook.jp>
2023-05-26 19:24:26 +0900

    add: textFieldControllerを用意した

--- lib/calculate_page.dart
+++ lib/calculate_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class CalculatePage extends StatelessWidget {
+class CalculatePage extends HookConsumerWidget {
   const CalculatePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final text = useState('');
+    final controller = useTextEditingController();
     return Scaffold(
       body: Center(
         child: Column(
@@ -15,12 +19,18 @@ class CalculatePage extends StatelessWidget {
               L10n.of(context)!.hello,
             ),
             TextField(
+              controller: controller,
               decoration: InputDecoration(
                   border: InputBorder.none, hintText: '整数を入力して'),
             ),
-            ElevatedButton(onPressed: () {}, child: Text("計算")),
+            ElevatedButton(
+                onPressed: () {
+                  text.value = controller.text;
+                  print('a');
+                },
+                child: Text("計算")),
             Text(
-              "答えが来るよ",
+              text.value,
             ),
           ],
         ),
