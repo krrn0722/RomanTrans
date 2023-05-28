6201d22a7a1bbe000a64751ccc03267c4110fa7d
krrn0722 <mur0722@outlook.jp>
2023-05-28 14:50:57 +0900

    add: エラー文のproviderを作成

---
+++ lib/error.dart
@@ -0,0 +1,10 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'error.g.dart';
+
+@riverpod
+class ErrorNotifier extends _$ErrorNotifier {
+  @override
+  String? build() {
+    return null;
+  }
+}
